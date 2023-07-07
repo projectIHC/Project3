b0b7fae8c79078efb5fbe7e260ea45d15dafefa8
Jean Paul Melendez Cabezas <jean.melendez@utec.edu.pe>
2023-07-07 16:41:40 -0500

    Interfaz

---
+++ menu_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/UnityEngine.UnityWebRequestWWWModule_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void UnityEngine.WWW::.ctor(System.String)
+extern void WWW__ctor_m5D29D83E9EE0925ED8252347CE24EC236401503D (void);
+// 0x00000002 System.String UnityEngine.WWW::get_text()
+extern void WWW_get_text_m95F7CAAC33FD0BAB9B535E3AEAFDA36B81B3EC4F (void);
+// 0x00000003 System.Void UnityEngine.WWW::LoadImageIntoTexture(UnityEngine.Texture2D)
+extern void WWW_LoadImageIntoTexture_m81D37261ED690E2548887532443C2DBEDCBE43D4 (void);
+// 0x00000004 System.String UnityEngine.WWW::get_url()
+extern void WWW_get_url_m368B1D7D23DC22E412A3F802C6E3047760665519 (void);
+// 0x00000005 System.Boolean UnityEngine.WWW::get_keepWaiting()
+extern void WWW_get_keepWaiting_m2D6B60FD9CB9C3E86D69E87EA32953AA68AE26B9 (void);
+// 0x00000006 System.Void UnityEngine.WWW::Dispose()
+extern void WWW_Dispose_mE5FC4A2013C63A68C287F139B177D86F75C4A74F (void);
+// 0x00000007 System.Boolean UnityEngine.WWW::WaitUntilDoneIfPossible()
+extern void WWW_WaitUntilDoneIfPossible_mD975AFF6737F00BB5003C5AEDBD795751F129A84 (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	WWW__ctor_m5D29D83E9EE0925ED8252347CE24EC236401503D,
+	WWW_get_text_m95F7CAAC33FD0BAB9B535E3AEAFDA36B81B3EC4F,
+	WWW_LoadImageIntoTexture_m81D37261ED690E2548887532443C2DBEDCBE43D4,
+	WWW_get_url_m368B1D7D23DC22E412A3F802C6E3047760665519,
+	WWW_get_keepWaiting_m2D6B60FD9CB9C3E86D69E87EA32953AA68AE26B9,
+	WWW_Dispose_mE5FC4A2013C63A68C287F139B177D86F75C4A74F,
+	WWW_WaitUntilDoneIfPossible_mD975AFF6737F00BB5003C5AEDBD795751F129A84,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	6613,
+	8133,
+	6613,
+	8133,
+	8023,
+	8273,
+	8023,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_UnityWebRequestWWWModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_UnityWebRequestWWWModule_CodeGenModule = 
+{
+	"UnityEngine.UnityWebRequestWWWModule.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
